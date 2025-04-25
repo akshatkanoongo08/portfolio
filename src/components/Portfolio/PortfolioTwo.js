20d5936a3997e26f888d8204ed483759abe345cf
akshatkanoongo08 <akshatkanoongo08@gmail.com>
2025-04-25 11:40:21 +0530

    only pdf not working

--- src/components/Portfolio/PortfolioTwo.js
+++ src/components/Portfolio/PortfolioTwo.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import api from "../../services/api";
+import "./PortfolioTwo.css";
 
 const API_URL = 'http://localhost:5000'; // Match your backend URL
 
